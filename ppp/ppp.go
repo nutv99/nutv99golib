c9456cb1a6e91d2dc715c8745b710c50a73f8d9f
nutv99 <nutv99@gmail.com>
2023-02-20 09:36:44 +0700

    Update ppp.go

--- ppp/ppp.go
+++ ppp/ppp.go
@@ -21,3 +21,15 @@ func Aaaa() string {
 	message := "Hello, "
 	return message
 }
+
+func listBook2sHandler(c *gin.Context) {
+	var book2s []Book2
+	if result := Db.Find(&book2s); result.Error != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"error": result.Error.Error(),
+		})
+		return
+	}
+
+	c.JSON(http.StatusOK, &book2s)
+}
