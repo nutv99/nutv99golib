d5209281b05330db5edaf534112e0d4905c8bb40
nutv99 <nutv99@gmail.com>
2023-02-20 07:18:11 +0700

    Add files via upload

---
+++ ppp/ppp.go
@@ -0,0 +1,12 @@
+package ppp
+
+type Book999 struct {
+	ID     string `json:"id"`
+	Title  string `json:"title"`
+	Author string `json:"author"`
+}
+
+func aaaa() string {
+	message := "Hello, "
+	return message
+}
