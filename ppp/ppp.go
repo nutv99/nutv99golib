1a1ef9abb7e0b373a9856bce52b3b6af6dedbc49
nutv99 <nutv99@gmail.com>
2023-02-20 07:32:38 +0700

    Update ppp.go

--- ppp/ppp.go
+++ ppp/ppp.go
@@ -6,6 +6,13 @@ type Book999 struct {
 	Author string `json:"author"`
 }
 
+
+type Book111 struct {
+	ID     string `json:"id"`
+	Title  string `json:"title"`
+	Author string `json:"author"`
+}
+
 func Aaaa() string {
 	message := "Hello, "
 	return message
